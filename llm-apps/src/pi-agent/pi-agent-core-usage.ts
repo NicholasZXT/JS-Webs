d1c36f88c0510f7ebfc298e8e538af308156b5d0
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2026-06-01 17:14:51 +0800

    Pi-Agents研究

---
+++ llm-apps/src/pi-agent/pi-agent-core-usage.ts
@@ -0,0 +1,4 @@
+import { Type, getModel, stream, Context, Tool } from "@earendil-works/pi-ai";
+import { 
+    Agent, AgentState, AgentMessage,
+} from "@earendil-works/pi-agent-core";
---
+++ llm-apps/src/pi-agent/pi-ai-usage.ts
@@ -0,0 +1,5 @@
+import {
+    Type, getProviders, getModels, getMode,
+    stream, complete, streamSimple, completeSimple,
+    Context, Tool, validateToolArguments, validateToolCall
+} from "@earendil-works/pi-ai";
